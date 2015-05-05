aa3ca1868d3f2947bf093237db92f5d05a398dbc
Maksim Lapin <mlapin@mpi-inf.mpg.de>
2015-05-05 22:47:45 +0200

    Revert "fix"
    This reverts commit 26a18aa399fddc1366fd0ff66edfd53823648936.

--- projections/topksimplexprojector.cpp
+++ projections/topksimplexprojector.cpp
@@ -44,7 +44,7 @@ template <typename RealType>
 bool TopKSimplexProjector<RealType>::CheckNeedFallback(
     const std::vector<RealType> x,
     const RealType t,
-    typename std::vector<RealType>::iterator &first) {
+    const typename std::vector<RealType>::iterator first) {
 
   RealType u = static_cast<RealType>(std::distance(x.begin(), first));
   RealType sum_u = std::accumulate(x.begin(), first, static_cast<RealType>(0));
