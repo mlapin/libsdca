f078699f9f44c2d037027d8a3f6286edd47b0691
Maksim Lapin <mlapin@mpi-inf.mpg.de>
2015-07-31 12:16:16 +0200

    added lambert_w_exp

---
+++ src/util/lambert.h
@@ -0,0 +1,83 @@
+#ifndef SDCA_UTIL_LAMBERT_H
+#define SDCA_UTIL_LAMBERT_H
+
+#include <cmath>
+#include <limits>
+
+namespace sdca {
+
+/**
+ * Omega constant, see
+ *    https://oeis.org/A030178.
+ * Omega = lambert_w(1); it is the solution to x * exp(x) = 1.
+ */
+const long double kOmega =
+0.5671432904097838729999686622103555497538157871865125081351310792230457930866L;
+
+/**
+ * Schroeder's / Householder's iteration of order 4 for the equation
+ *    w - z * exp(-w) = 0.
+ * Expected convergence rate of order 5, see
+ * [1] A. Householder, The numerical treatment of a single nonlinear equation.
+ *     McGraw-Hill, 1970.
+ * [2] T. Fukushima, Precise and fast computation of Lambert W-functions
+ *     without transcendental function evaluations.
+ *     Journal of Computational and Applied Mathematics 244 (2013): 77-89.
+ *
+ * Input: w = w_n, y = z * exp(-w_n).
+ * Returns: w_{n+1}.
+ **/
+template <typename Type>
+inline Type
+lambert_w_householder_4(
+    const Type w,
+    const Type y
+  ) {
+  Type f0 = w - y, f1 = static_cast<Type>(1) + y;
+  Type f11 = f1 * f1, f0y = f0 * y;
+  Type f00y = f0 * f0y;
+  return w - static_cast<Type>(4) * f0 * (
+      static_cast<Type>(6) * f1 * (f11 + f0y) + f00y
+    ) / (
+      f11 * (static_cast<Type>(24) * f11 + static_cast<Type>(36) * f0y) +
+      f00y * (static_cast<Type>(14) * y + f0 + static_cast<Type>(8))
+    );
+}
+
+/**
+ * Lambert W function of exp(x),
+ *    w = W_0(exp(x)).
+ * Computed w satisfies the equation
+ *    w + ln(w) = x.
+ **/
+template <typename Type>
+inline Type
+lambert_w_exp(
+    const Type x
+  ) {
+  Type w, y, w1(0);
+  if (x > std::numeric_limits<Type>::epsilon()) {
+    // x > 0, w \approx x, w_0 = x, y_0 = exp(x - x) = 1
+    w = x;
+    y = 1;
+  } else if (x < - std::numeric_limits<Type>::epsilon()) {
+    // x < 0, ln(w) \approx x, w_0 = exp(x), y_0 = exp(x - w_1)
+    if (x < static_cast<Type>(500)) return static_cast<Type>(0);
+    w = std::exp(x);
+    y = std::exp(x - w);
+  } else {
+    // x = 0, w = Omega
+    return static_cast<Type>(kOmega);
+  }
+  for (;;) {
+    w1 = lambert_w_householder_4(w, y);
+    if (w1 == w) break;
+    w = w1;
+    y = std::exp(x - w);
+  }
+  return w;
+}
+
+}
+
+#endif
