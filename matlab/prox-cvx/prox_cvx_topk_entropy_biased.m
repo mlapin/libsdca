79763a27d14ecd5557d75977aeba42688965d23a
Maksim Lapin <mlapin@mpi-inf.mpg.de>
2015-08-29 20:15:25 +0200

    prox topk entropy biased fixed

---
+++ matlab/prox-cvx/prox_cvx_topk_entropy_biased.m
@@ -0,0 +1,24 @@
+function [X,lambda,nu,mu] = prox_cvx_topk_entropy_biased(A, opts)
+%#ok<*ASGLU>
+%#ok<*EQEFF>
+%#ok<*STOUT>
+%#ok<*VUNUS>
+
+k = opts.k;
+alpha = opts.alpha;
+
+[d,n] = size(A);
+
+cvx_begin %quiet
+  cvx_expert true;
+  cvx_precision high;
+  variable X(d,n);
+  dual variable mu;
+  dual variable nu;
+  dual variable lambda;
+  minimize( 0.5*alpha*(sum(X(:).^2) + sum(sum(X,1).^2)) ...
+    - A(:)'*X(:) - sum(entr(X(:))) - sum(entr(1 - sum(X,1))) );
+  mu: 0 <= X;
+  nu: X <= ones(d)*X/k;
+  lambda: sum(X,1) <= 1;
+cvx_end
