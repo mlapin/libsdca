9a8e0364a5eee8c4817565a5265d2cc509f4c876
Maksim Lapin <mlapin@mpi-inf.mpg.de>
2015-11-18 11:00:10 +0100

    matlab blas

--- src/util/blas.h
+++ src/util/blas.h
@@ -8,20 +8,17 @@
   #include <Accelerate/Accelerate.h>
 #elif defined(BLAS_MATLAB)
   #include <blas.h>
-  #include <cstddef>
-  typedef std::ptrdiff_t ptrdiff_t; // Matlab does not define ptrdiff_t
 #elif defined(BLAS_DEFAULT)
   #include <cblas.h>
 #elif defined(BLAS_DEFAULT_LOCAL_HEADER)
   #include "cblas.h"
 #endif
 
-#if !defined(CBLAS_ORDER)
-enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
-#endif
-
-#if !defined(CBLAS_TRANSPOSE)
-enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
+#if defined(BLAS_MATLAB)
+  #include <cstddef>
+  typedef std::ptrdiff_t ptrdiff_t; // required for Matlab's blas.h
+  enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102};
+  enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113};
 #endif
 
 namespace sdca {
@@ -40,11 +37,11 @@ sdca_blas_scal(
     const float alpha,
     float* X
   ) {
-#if defined(cblas_sscal)
-  cblas_sscal(n, alpha, X, 1);
-#else
+#if defined(sscal)
   blas_int inc(1);
   sscal(const_cast<blas_int*>(&n), const_cast<float*>(&alpha), X, &inc);
+#else
+  cblas_sscal(n, alpha, X, 1);
 #endif
 }
 
@@ -54,11 +51,11 @@ sdca_blas_scal(
     const double alpha,
     double* X
   ) {
-#if defined(cblas_dscal)
-  cblas_dscal(n, alpha, X, 1);
-#else
+#if defined(dscal)
   blas_int inc(1);
   dscal(const_cast<blas_int*>(&n), const_cast<double*>(&alpha), X, &inc);
+#else
+  cblas_dscal(n, alpha, X, 1);
 #endif
 }
 
@@ -68,11 +65,11 @@ sdca_blas_copy(
     const float* X,
     float* Y
   ) {
-#if defined(cblas_scopy)
-  cblas_scopy(n, X, 1, Y, 1);
-#else
+#if defined(scopy)
   blas_int inc(1);
   scopy(const_cast<blas_int*>(&n), const_cast<float*>(X), &inc, Y, &inc);
+#else
+  cblas_scopy(n, X, 1, Y, 1);
 #endif
 }
 
@@ -82,11 +79,11 @@ sdca_blas_copy(
     const double* X,
     double* Y
   ) {
-#if defined(cblas_dcopy)
-  cblas_dcopy(n, X, 1, Y, 1);
-#else
+#if defined(dcopy)
   blas_int inc(1);
   dcopy(const_cast<blas_int*>(&n), const_cast<double*>(X), &inc, Y, &inc);
+#else
+  cblas_dcopy(n, X, 1, Y, 1);
 #endif
 }
 
@@ -97,12 +94,12 @@ sdca_blas_axpy(
     const float* X,
     float* Y
   ) {
-#if defined(cblas_saxpy)
-  cblas_saxpy(n, alpha, X, 1, Y, 1);
-#else
+#if defined(saxpy)
   blas_int inc(1);
   saxpy(const_cast<blas_int*>(&n), const_cast<float*>(&alpha),
         const_cast<float*>(X), &inc, Y, &inc);
+#else
+  cblas_saxpy(n, alpha, X, 1, Y, 1);
 #endif
 }
 
@@ -113,12 +110,12 @@ sdca_blas_axpy(
     const double* X,
     double* Y
   ) {
-#if defined(cblas_daxpy)
-  cblas_daxpy(n, alpha, X, 1, Y, 1);
-#else
+#if defined(daxpy)
   blas_int inc(1);
   daxpy(const_cast<blas_int*>(&n), const_cast<double*>(&alpha),
         const_cast<double*>(X), &inc, Y, &inc);
+#else
+  cblas_daxpy(n, alpha, X, 1, Y, 1);
 #endif
 }
 
@@ -132,14 +129,14 @@ sdca_blas_axpby(
   ) {
 #if defined(cblas_saxpby)
   cblas_saxpby(n, alpha, X, 1, beta, Y, 1);
-#elif defined(cblas_sscal)
-  cblas_sscal(n, beta, Y, 1);
-  cblas_saxpy(n, alpha, X, 1, Y, 1);
-#else
+#elif defined(sscal)
   blas_int inc(1);
   sscal(const_cast<blas_int*>(&n), const_cast<float*>(&beta), Y, &inc);
   saxpy(const_cast<blas_int*>(&n), const_cast<float*>(&alpha),
         const_cast<float*>(X), &inc, Y, &inc);
+#else
+  cblas_sscal(n, beta, Y, 1);
+  cblas_saxpy(n, alpha, X, 1, Y, 1);
 #endif
 }
 
@@ -153,14 +150,14 @@ sdca_blas_axpby(
   ) {
 #if defined(cblas_daxpby)
   cblas_daxpby(n, alpha, X, 1, beta, Y, 1);
-#elif defined(cblas_dscal)
-  cblas_dscal(n, beta, Y, 1);
-  cblas_daxpy(n, alpha, X, 1, Y, 1);
-#else
+#elif defined(dscal)
   blas_int inc(1);
   dscal(const_cast<blas_int*>(&n), const_cast<double*>(&beta), Y, &inc);
   daxpy(const_cast<blas_int*>(&n), const_cast<double*>(&alpha),
         const_cast<double*>(X), &inc, Y, &inc);
+#else
+  cblas_dscal(n, beta, Y, 1);
+  cblas_daxpy(n, alpha, X, 1, Y, 1);
 #endif
 }
 
@@ -170,12 +167,12 @@ sdca_blas_dot(
     const float* X,
     const float* Y
   ) {
-#if defined(cblas_sdot)
-  return cblas_sdot(n, X, 1, Y, 1);
-#else
+#if defined(sdot)
   blas_int inc(1);
   return sdot(const_cast<blas_int*>(&n), const_cast<float*>(X), &inc,
               const_cast<float*>(Y), &inc);
+#else
+  return cblas_sdot(n, X, 1, Y, 1);
 #endif
 }
 
@@ -185,12 +182,12 @@ sdca_blas_dot(
     const double* X,
     const double* Y
   ) {
-#if defined(cblas_ddot)
-  return cblas_ddot(n, X, 1, Y, 1);
-#else
+#if defined(ddot)
   blas_int inc(1);
   return ddot(const_cast<blas_int*>(&n), const_cast<double*>(X), &inc,
               const_cast<double*>(Y), &inc);
+#else
+  return cblas_ddot(n, X, 1, Y, 1);
 #endif
 }
 
@@ -199,11 +196,11 @@ sdca_blas_asum(
     const blas_int n,
     const float* X
   ) {
-#if defined(cblas_sasum)
-  return cblas_sasum(n, X, 1);
-#else
+#if defined(sasum)
   blas_int inc(1);
   return sasum(const_cast<blas_int*>(&n), const_cast<float*>(X), &inc);
+#else
+  return cblas_sasum(n, X, 1);
 #endif
 }
 
@@ -212,11 +209,11 @@ sdca_blas_asum(
     const blas_int n,
     const double* X
   ) {
-#if defined(cblas_dasum)
-  return cblas_dasum(n, X, 1);
-#else
+#if defined(dasum)
   blas_int inc(1);
   return dasum(const_cast<blas_int*>(&n), const_cast<double*>(X), &inc);
+#else
+  return cblas_dasum(n, X, 1);
 #endif
 }
 
@@ -225,11 +222,11 @@ sdca_blas_nrm2(
     const blas_int n,
     const float* X
   ) {
-#if defined(cblas_snrm2)
-  return cblas_snrm2(n, X, 1);
-#else
+#if defined(snrm2)
   blas_int inc(1);
   return snrm2(const_cast<blas_int*>(&n), const_cast<float*>(X), &inc);
+#else
+  return cblas_snrm2(n, X, 1);
 #endif
 }
 
@@ -238,11 +235,11 @@ sdca_blas_nrm2(
     const blas_int n,
     const double* X
   ) {
-#if defined(cblas_dnrm2)
-  return cblas_dnrm2(n, X, 1);
-#else
+#if defined(dnrm2)
   blas_int inc(1);
   return dnrm2(const_cast<blas_int*>(&n), const_cast<double*>(X), &inc);
+#else
+  return cblas_dnrm2(n, X, 1);
 #endif
 }
 
@@ -257,16 +254,16 @@ sdca_blas_gemv(
     const float alpha = 1,
     const float beta = 0
     ) {
-#if defined(cblas_sgemv)
-  cblas_sgemv(CblasColMajor, transA, m, n, alpha, A, m,
-              X, 1, beta, Y, 1);
-#else
+#if defined(sgemv)
   blas_int inc(1);
   char trans((transA == CblasNoTrans) ? 'N' : 'T');
   sgemv(&trans, const_cast<blas_int*>(&m), const_cast<blas_int*>(&n),
         const_cast<float*>(&alpha),
         const_cast<float*>(A), const_cast<blas_int*>(&m),
         const_cast<float*>(X), &inc, const_cast<float*>(&beta), Y, &inc);
+#else
+  cblas_sgemv(CblasColMajor, transA, m, n, alpha, A, m,
+              X, 1, beta, Y, 1);
 #endif
 }
 
@@ -281,16 +278,16 @@ sdca_blas_gemv(
     const double alpha = 1,
     const double beta = 0
     ) {
-#if defined(cblas_dgemv)
-  cblas_dgemv(CblasColMajor, transA, m, n, alpha, A, m,
-              X, 1, beta, Y, 1);
-#else
+#if defined(dgemv)
   blas_int inc(1);
   char trans((transA == CblasNoTrans) ? 'N' : 'T');
   dgemv(&trans, const_cast<blas_int*>(&m), const_cast<blas_int*>(&n),
         const_cast<double*>(&alpha),
         const_cast<double*>(A), const_cast<blas_int*>(&m),
         const_cast<double*>(X), &inc, const_cast<double*>(&beta), Y, &inc);
+#else
+  cblas_dgemv(CblasColMajor, transA, m, n, alpha, A, m,
+              X, 1, beta, Y, 1);
 #endif
 }
 
@@ -303,13 +300,13 @@ sdca_blas_ger(
     const float* Y,
     float* A
     ) {
-#if defined(cblas_sger)
-  cblas_sger(CblasColMajor, m, n, alpha, X, 1, Y, 1, A, m);
-#else
+#if defined(sger)
   blas_int inc(1);
   sger(const_cast<blas_int*>(&m), const_cast<blas_int*>(&n),
        const_cast<float*>(&alpha), const_cast<float*>(X), &inc,
        const_cast<float*>(Y), &inc, A, const_cast<blas_int*>(&m));
+#else
+  cblas_sger(CblasColMajor, m, n, alpha, X, 1, Y, 1, A, m);
 #endif
 }
 
@@ -322,13 +319,13 @@ sdca_blas_ger(
     const double* Y,
     double* A
     ) {
-#if defined(cblas_dger)
-  cblas_dger(CblasColMajor, m, n, alpha, X, 1, Y, 1, A, m);
-#else
+#if defined(dger)
   blas_int inc(1);
   dger(const_cast<blas_int*>(&m), const_cast<blas_int*>(&n),
        const_cast<double*>(&alpha), const_cast<double*>(X), &inc,
        const_cast<double*>(Y), &inc, A, const_cast<blas_int*>(&m));
+#else
+  cblas_dger(CblasColMajor, m, n, alpha, X, 1, Y, 1, A, m);
 #endif
 }
 
@@ -347,10 +344,7 @@ sdca_blas_gemm(
     const float alpha = 1,
     const float beta = 0
     ) {
-#if defined(cblas_sgemm)
-  cblas_sgemm(CblasColMajor, transA, transB, m, n, k,
-              alpha, A, lda, B, ldb, beta, C, m);
-#else
+#if defined(sgemm)
   char transa((transA == CblasNoTrans) ? 'N' : 'T');
   char transb((transB == CblasNoTrans) ? 'N' : 'T');
   sgemm(&transa, &transb, const_cast<blas_int*>(&m), const_cast<blas_int*>(&n),
@@ -358,6 +352,9 @@ sdca_blas_gemm(
         const_cast<float*>(A), const_cast<blas_int*>(&lda),
         const_cast<float*>(B), const_cast<blas_int*>(&ldb),
         const_cast<float*>(&beta), C, const_cast<blas_int*>(&m));
+#else
+  cblas_sgemm(CblasColMajor, transA, transB, m, n, k,
+              alpha, A, lda, B, ldb, beta, C, m);
 #endif
 }
 
@@ -376,10 +373,7 @@ sdca_blas_gemm(
     const double alpha = 1,
     const double beta = 0
     ) {
-#if defined(cblas_dgemm)
-  cblas_dgemm(CblasColMajor, transA, transB, m, n, k,
-              alpha, A, lda, B, ldb, beta, C, m);
-#else
+#if defined(dgemm)
   char transa((transA == CblasNoTrans) ? 'N' : 'T');
   char transb((transB == CblasNoTrans) ? 'N' : 'T');
   dgemm(&transa, &transb, const_cast<blas_int*>(&m), const_cast<blas_int*>(&n),
@@ -387,6 +381,9 @@ sdca_blas_gemm(
         const_cast<double*>(A), const_cast<blas_int*>(&lda),
         const_cast<double*>(B), const_cast<blas_int*>(&ldb),
         const_cast<double*>(&beta), C, const_cast<blas_int*>(&m));
+#else
+  cblas_dgemm(CblasColMajor, transA, transB, m, n, k,
+              alpha, A, lda, B, ldb, beta, C, m);
 #endif
 }
 
